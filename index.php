91eb2a3774daee64df89a74fc85521c0c0f7b40f
Lumetas <pokh9864@gmail.com>
2025-03-14 20:41:42 +0500

    example global

--- index.php
+++ index.php
@@ -8,17 +8,17 @@ Route::setServer(new Server('0.0.0.0', 80));
 
 Route::get('/', function(Request $rq) {
 	$GLOBALS["i"] += 1;
-    return new Response("is a main page $GLOBALS[i]", 200);
+	return new Response("is a main page $GLOBALS[i]", 200);
 });
 
 Route::get('/test', function(Request $rq) {
-    return new Response("is a test page", 200);
+	return new Response("is a test page", 200);
 });
 
 Route::notFound(function(Request $rq) {
-    return new Response('is a "TI DOLBOEB PAGE NOT FOUND"', 404);
+	return new Response('is a "TI DOLBOEB PAGE NOT FOUND"', 404);
 });
 
 Route::startHandling(function () {
-    echo "Server started!\n";
+	echo "Server started!\n";
 });
