ae803645c74a5670ab3d3cae25d5c23f7e0c503f
Lumetas <pokh9864@gmail.com>
2025-03-16 21:13:19 +0500

    database, workers and fix ghost request in server

--- framework/core/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-abstract class Controller {
-	public static function prepare_html (string $html) : Response{
-		$r = new Response($html, 200);
-		$r->header("Content-Type", "text/html");
-		return $r;
-	}
-}
-
